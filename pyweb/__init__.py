d0070c7acf4c21c19e99c293ca7f66728a409674
shell <shell@hpmini>
2010-10-27 01:27:36 +0800

    ??EventletClient????

--- pyweb/__init__.py
+++ pyweb/__init__.py
@@ -9,6 +9,7 @@ from actions import MemcacheSession, MongoSession
 from basesock import *
 from basehttp import get_params_dict
 from daemon import Daemon
+from evlet import EventletClient
 from fcgi import FcgiServer
 from files import StaticFile, TemplateFile
 from http import HttpServer
