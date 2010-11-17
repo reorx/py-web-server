71b43b05a799a69551975e3b0681251bbc109654
shell <shell@proxy>
2010-11-17 15:30:33 +0800

    ???????

--- pyweb/ebus.py
+++ pyweb/ebus.py
@@ -16,10 +16,12 @@ try:
     import epoll
     epoll_factory = epoll.poll
     timout_factor = 1000
+    python_epoll = True
 except ImportError:
     import select as epoll
     epoll_factory = epoll.epoll
     timout_factor = 1.0
+    python_epoll = False
 
 class TimeOutException(Exception): pass
 
@@ -31,15 +33,20 @@ class EpollBus(object):
         def __cmp__(self, o): return self.timeout > o.timeout
 
     def __init__(self):
-        self.poll = epoll_factory()
         self.fdmap, self.queue, self.timeline = {}, [], []
 
+    def init_poll(self):
+        self.poll = epoll_factory()
+        if not python_epoll: self._epoll_modify = self.poll.modify
+        else: self._epoll_modify = self.poll.register
+
     def register(self, fd, ev):
-        if fd in self.fdmap: self.poll.modify(fd, ev)
-        else: self.poll.register(fd, ev)
+        if fd in self.fdmap: self._epoll_modify(fd, ev | epoll.POLLHUP)
+        else: self.poll.register(fd, ev | epoll.POLLHUP)
         self.fdmap[fd] = greenlet.getcurrent()
 
     def unregister(self, fd):
+        if fd == -1: return
         try: del self.fdmap[fd]
         except KeyError: pass
         try: self.poll.unregister(fd)
@@ -57,9 +64,11 @@ class EpollBus(object):
             heapq.heapify(self.timeline)
         except ValueError: pass
 
-    def next_job(self, gr): self.queue.append(gr)
+    def next_job(self, gr):
+        self.queue.append(gr)
+        return len(self.queue) > 50
 
-    def _switch_queue(self):
+    def switch_queue(self):
         gr = greenlet.getcurrent()
         while self.queue:
             q = self.queue[-1]
@@ -67,32 +76,32 @@ class EpollBus(object):
             elif q == gr: return self.queue.pop()
             else: q.switch()
 
+    def load_poll(self):
+        if not self.timeline: timeout = -1
+        else:
+            timeout = self.timeline[0].timeout - time.time()
+            timeout *= timout_factor
+        for fd, ev in self.poll.poll(timeout):
+            # print 'event come'
+            if fd not in self.fdmap: self.poll.unregister(fd)
+            elif ev == epoll.POLLHUP:
+                self.fdmap[fd].throw(EOFError)
+                self.unregister(fd)
+            elif self.queue.append(self.fdmap[fd]): break
+        # print len(self.queue), len(self.fdmap)
+        while self.timeline and time.time() > self.timeline[0].timeout:
+            next = heapq.heappop(self.timeline)
+            next.gr.throw(next.exp)
+        return bool(self.queue)
+
     def switch(self):
-        while not self._switch_queue():
-            if not self.timeline: timeout = -1
-            else:
-                timeout = self.timeline[0].timeout - time.time()
-                timeout *= timout_factor
-            for fd, ev in self.poll.poll(timeout):
-                if fd not in self.fdmap:
-                    self.poll.unregister(fd)
-                    continue
-                print 'event come'
-                self.queue.append(self.fdmap[fd])
-                if len(self.queue) > 50: break
-            print len(self.queue), len(self.fdmap)
-            while self.timeline and time.time() > self.timeline[0].timeout:
-                next = heapq.heappop(self.timeline)
-                next.gr.throw(next.exp)
+        while not self.switch_queue(): self.load_poll()
 
 bus = EpollBus()
 
 class TokenPool(object):
 
-    def __init__(self, min_item, max_item):
-        self.min_item, self.max_item = min_item, max_item
-        self.token, self.gr_wait = min_item, []
-
+    def __init__(self, max_item): self.token, self.gr_wait = max_item, []
     @contextmanager
     def item(self):
         gr = greenlet.getcurrent()
@@ -109,8 +118,8 @@ class TokenPool(object):
 
 class ObjPool(object):
 
-    def __init__(self, min_item, max_item):
-        self.min_item, self.max_item = min_item, max_item
+    def __init__(self, max_item):
+        self.max_item = max_item
         self.pool, self.count, self.gr_wait = [], 0, []
 
     @contextmanager
@@ -124,10 +133,9 @@ class ObjPool(object):
         obj = self.pool.pop()
         try: yield obj
         finally:
+            self.free(obj)
             self.pool.append(obj)
             self.count -= 1
             if self.count == self.max_item - 1:
                 bus.next_job(greenlet.getcurrent())
                 self.gr_wait.pop().switch()
-            elif self.count > self.min_item and hasattr(self, 'free'):
-                while self.pool: self.free(self.pool.pop())
